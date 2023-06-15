594d0eda1a62800c68a51f9eb5f003b2ef0bfeeb
diparktech <kimjumia@gmail.com>
2023-06-15 09:46:34 -0700

    corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * mainWrite a C program that prints ex
- * Return 0 : (success)
+ * main - Write a C program that prints ex
+ * Return: 0  (success)
  */
-int main (void)
+int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
