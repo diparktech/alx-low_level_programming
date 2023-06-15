97adeb98fcc968facb48018a0f2c0a7ae22def57
diparktech <kimjumia@gmail.com>
2023-06-15 10:15:27 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- *main - a program that prints using printf
- *Function
- *Return: 0 (succ) 
- **/
+* main - a program that prints using printf
+
+* Function
+* Return: 0 (succ) 
+**/
 int main(void)
 {
-prinf("with proper grammar, but the outcome is a piece of art,\n");
+prinf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
