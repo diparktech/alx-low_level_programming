61ae3e1ea56e1cab5c9996462116b56f470c21dd
diparktech <kimjumia@gmail.com>
2023-06-15 12:05:26 -0700

    repurpose printf

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print an output
+ * function - will use a void main
+ * return (0) - the return value
+ **/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
