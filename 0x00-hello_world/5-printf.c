b8903b4aee4f3ccc0c150ad0d2d57f39283d4336
diparktech <kimjumia@gmail.com>
2023-06-15 10:05:12 -0700

    some eroros

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-/** 
- * main - a program that prints using printf
+/**
+ *main - a program that prints using printf
  *Function
- * Return: 0 (succ) 
- * */
+ *Return: 0 (succ) 
+ **/
 int main(void)
 {
 prinf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
