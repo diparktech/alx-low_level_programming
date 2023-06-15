042bba6a7654eaa3dd8bb4efb673497557aba5e7
diparktech <kimjumia@gmail.com>
2023-06-15 09:33:05 -0700

    print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * mainWrite a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
+ * return 0 : (successful execution)
+ */
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
