de58d06bb672a05e0e47cad2723cf27c661ef882
diparktech <kimjumia@gmail.com>
2023-06-15 13:45:59 -0700

    correct code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-* main - a program that prints using printf
-* Function
-* Return: 0 (succ) 
-**/
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-prinf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
