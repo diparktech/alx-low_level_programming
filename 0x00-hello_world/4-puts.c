dd129bb7c5f9367fcf848367a6070bf6345c1f8b
diparktech <kimjumia@gmail.com>
2023-06-15 09:37:51 -0700

    puts printer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * mainWrite a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
- * return 0 : (successful execution)
+ * mainWrite a C program that prints ex
+ * Return 0 : (success)
  */
 int main (void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
