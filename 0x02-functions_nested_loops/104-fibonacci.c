f791eb2982c33e8a15b95737d0bc408197ea1551
diparktech <kimjumia@gmail.com>
2023-06-21 09:12:11 -0700

    fibonacii

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,77 +3,30 @@
 /**
  * main - fibonacci <3
  *
- * Purpose - no hardcode
- *
- * Return:  (Success)
+ * Purple
+ * return -0 success
  */
+#include <stdio.h>
 
-int main(void)
-{
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+int main() {
+    int fib1 = 1;  // First Fibonacci number
+    int fib2 = 2;  // Second Fibonacci number
+    int count = 2; // Counter for the number of Fibonacci numbers found
 
-	printf("%lu", bef);
+    printf("%d, %d, ", fib1, fib2); // Print the first two Fibonacci numbers
 
-	for (i = 1; i < 91; i++)
-	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
+    while (count < 98) {
+        int nextFib = fib1 + fib2;  // Calculate the next Fibonacci number
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+        printf("%d, ", nextFib); // Print the next Fibonacci number
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
-	}
-	printf("\n");
-	return (0);
+        fib1 = fib2; // Update the values for the next iteration
+        fib2 = nextFib;
+
+        count++; // Increment the counter
+    }
+
+    printf("\n"); // Print a new line
+
+    return 0;
 }
-#include "main.h"
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-#ifndef main_h
-#define main_h
-
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
-
