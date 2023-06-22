19122e4134ab9355568e903f0048049d82ea2de4
diparktech <kimjumia@gmail.com>
2023-06-22 11:04:10 -0700

    missing main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 /**
 * main - causes an infinite loop
-* while - loop statement
-* putchar -output
-* \\o/\n  -next line
-* \n -next line
-* printf -displaly result
-* return: 0
+* Return: 0
 */
 int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n");
-/**
-*i = 0;
-* while (i < 10)
-*{
-*putchar(i);
-*}
-*/
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
+
