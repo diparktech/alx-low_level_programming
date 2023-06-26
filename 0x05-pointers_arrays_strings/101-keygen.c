a0952527e2793bbb215b9788746234750d3dd374
diparktech <134946907+diparktech@users.noreply.github.com>
2023-06-26 18:20:58 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,13 +9,8 @@
  */
 int main(void)
 {
-	int pass[100];
+	int pass[100]; 	int i, sum, n; 	sum = 0; srand(time(NULL));
 	
-	int i, sum, n;
-	
-	sum = 0;
-	
-	srand(time(NULL));
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
