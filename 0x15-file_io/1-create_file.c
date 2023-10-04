b31ef6c9ce259eaa8151f0a8b15e2983a689a8a3
diparktech <134946907+diparktech@users.noreply.github.com>
2023-10-04 12:31:29 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * create_file - new file.
  * @filename: A pointer to the  file to be create.
  * @text_content: A pointer to  string.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+  * Return: If the function fails - -1.
+ * Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
